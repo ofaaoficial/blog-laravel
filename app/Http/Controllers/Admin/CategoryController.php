e378ce3a9bef50704e277d071ee93fda56ec22fc
ofaa <oscarfamado@gmail.com>
2018-04-24 15:34:13 -0500

    feat (start) : The project finished.

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\CategoryStoreRequest;
+use App\Http\Requests\CategoryUpdateRequest;
+use App\Http\Controllers\Controller;
+use App\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Category::orderBy('id', 'DESC')->paginate(10);
+        return view('admin.categories.index', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CategoryStoreRequest $request)
+    {
+        $category = Category::create($request->all());
+
+        return redirect()->route('categories.edit', $category->id)
+        ->with('info', 'Categoria creada correctamente.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $category = Category::find($id);
+        return view('admin.categories.show', compact('category'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $category = Category::find($id);
+        return view('admin.categories.edit', compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(CategoryUpdateRequest $request, $id)
+    {
+        $category = Category::find($id);
+        $category->fill($request->all())->save();
+        return redirect()->route('categories.edit', $category->id)
+        ->with('info', 'Categoria actualizada correctamente.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Category::find($id)->delete();
+        return back()->with('info', "La categoria ha sido eliminada correctamente." );
+    }
+}
---
+++ resources/views/admin/categories/partials/form.blade.php
@@ -0,0 +1,27 @@
+<div class="form-group">
+	{{ Form::label('name', 'Nombre de la etiqueta') }}
+	{{ Form::text('name', null, ['class' => 'form-control', 'id' => 'name']) }}
+</div>
+<div class="form-group">
+	{{ Form::label('slug', 'URL Amigable') }}
+	{{ Form::text('slug', null, ['class' => 'form-control', 'id' => 'slug']) }}
+</div>
+<div class="form-group">
+	{{ Form::label('body', 'Descripción') }}
+	{{ Form::textarea('body', null, ['class' => 'form-control']) }}
+</div>
+<div class="form-group">
+	{{ Form::submit('Guardar', ['class' => 'btn btn-sm btn-success'])}}
+</div>
+@section('scripts')
+<script src="{{ asset('vendor/stringToSlug/stringToSlug.min.js') }}"></script>
+<script>
+	$(document).ready(function(){
+		$('#name, #slug').stringToSlug({
+			callback : function(text){
+				$('#slug').val(text);
+			}
+		});
+	});
+</script>
+@endsection
